8316bfd9cdecb46d4dea0a715eb909e549848aa6
michelefant <hellomichelefant@gmail.com>
2017-03-26 12:45:42 -0700

    index & resume fixed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
-// Make it rain!
+// When mouse over #name, 
+	// MICHELLE TANG expand to MENGSHUANG MICHELLE TANG
+	// h2 slide down
+
+// When mouse over cover img
+	// opacity of the img become 40% (already did it in CSS)
+	// .hidden_text shows up in the middle of the img one line after another
+
+
+
+
 $(document).ready(function(){
   $(".hamburger").click(function(){
     $(this).toggleClass("is-active");
   });
 });
+
