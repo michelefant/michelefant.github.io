dfa25803852b6d45ae78d99e64611c135830c947
michelefant <hellomichelefant@gmail.com>
2017-04-02 12:14:35 -0700

    header animation done

--- js/main.js
+++ js/main.js
@@ -7,11 +7,46 @@
 	// .hidden_text shows up in the middle of the img one line after another
 
 
-
-
 $(document).ready(function(){
-  $(".hamburger").click(function(){
-    $(this).toggleClass("is-active");
+  
+  var $randomnbr = $('.nbr');
+  var $timer= 6;
+  var $it;
+  var $data = 0;
+  var index;
+  var change;
+  var letters = ["m", "i", "c", "h", "e", "l", "l", "e", "t", "a", "n", "g"];
+  
+  $randomnbr.each(function(){
+      
+    change = Math.round(Math.random()*100);
+    $(this).attr('data-change', change);
+    
   });
+  
+  function random(){
+    return Math.round(Math.random()*9);
+  };
+  
+  function select(){
+    return Math.round(Math.random()*$randomnbr.length+1);
+  };
+  
+  function value(){
+    $('.nbr:nth-child('+select()+')').html(''+random()+'');
+    $('.nbr:nth-child('+select()+')').attr('data-number', $data);
+    $data++;
+    
+    $randomnbr.each(function(){
+        if(parseInt($(this).attr('data-number')) > parseInt($(this).attr('data-change'))){
+          index = $('.ltr').index(this);
+          $(this).html(letters[index]);
+          $(this).removeClass('nbr');
+        }
+    });
+    
+  };
+  
+  $it = setInterval(value, $timer);
+    
 });
-
